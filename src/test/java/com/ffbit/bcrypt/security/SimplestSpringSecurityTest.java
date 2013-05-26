eda1897a19ac0e5ba08554ec4562b034644ecba5
Dmytro Chyzhykov <dmytro.chyzhykov@yandex.ru>
2013-05-26 23:53:04 +0300

    Spring Security Mock Authentication.

--- src/test/java/com/ffbit/bcrypt/security/SimplestSpringSecurityTest.java
+++ src/test/java/com/ffbit/bcrypt/security/SimplestSpringSecurityTest.java
@@ -3,15 +3,21 @@ package com.ffbit.bcrypt.security;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mock.web.MockHttpSession;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.FilterChainProxy;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.web.context.WebApplicationContext;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -28,8 +34,6 @@ public class SimplestSpringSecurityTest {
     @Autowired
     private MockMvc mockMvc;
 
-    private ResultActions resultActions;
-
     private final String SECURED_URI = "/";
 
     private final String LOGIN_PAGE = "http://localhost/spring_security_login";
@@ -41,6 +45,30 @@ public class SimplestSpringSecurityTest {
         mockMvc.perform(get(SECURED_URI))
                 .andExpect(status().isFound())
                 .andExpect(redirectedUrl(LOGIN_PAGE));
+    }
+
+    @Test
+    public void itShouldDenyAccessForNotRegistedUser() throws Exception {
+        mockMvc.perform(post(LOGIN_PROCESSING_URI)
+                .param("j_username", "bad_user")
+                .param("j_password", "bad_password"))
+                .andExpect(status().isFound());
+        mockMvc.perform(get(SECURED_URI))
+                .andExpect(redirectedUrl(LOGIN_PAGE));
+    }
+
+    @Test
+    public void itShouldAllowAccessRoleUser() throws Exception {
+        Authentication authentication = new UsernamePasswordAuthenticationToken("user", "user_pwd");
+        SecurityContext securityContext = SecurityContextHolder.getContext();
+        securityContext.setAuthentication(authentication);
+
+        MockHttpSession session = new MockHttpSession();
+        session.setAttribute(
+                HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY,
+                securityContext);
+
+        mockMvc.perform(get(SECURED_URI).session(session)).andExpect(status().isOk());
     }
 
 }
